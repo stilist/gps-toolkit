4239c25cf2ac0113e435769c5563dac8d6a18b98
Jordan Cole <stilist@ratafia.info>
2016-06-04 22:44:09 -0700

    Export GoldCode from index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import 'babel-polyfill'
 
 export { FibonacciLFSR } from './fibonacci-lfsr'
 export { GaloisLFSR } from './galois-lfsr'
+export { GoldCode } from './gold-code'
 export { MSequence } from './m-sequence'
 export { LFSR } from './lfsr'
