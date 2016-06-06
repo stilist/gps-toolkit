70b8eb5b8baf70629250c5c6c07239cd0cfae6d6
Jordan Cole <stilist@ratafia.info>
2016-06-05 19:11:47 -0700

    Add cross-correlation utility

---
+++ src/utilities/cross-correlate.js
@@ -0,0 +1,66 @@
+import 'babel-polyfill'
+import dot_product from './dot-product'
+
+/**
+ * Rotate `distance` members to the head of `sequence`.
+ *
+ * @param {number[]} sequence - The array to rotate.
+ * @param {number} [distance=1] - How many members to move.
+ *
+ * @access private
+ * @throws {TypeError}
+ *
+ * @todo Handle `distance > sequence.length`.
+ */
+function rotate_sequence(sequence, distance = 1) {
+  if (!sequence.length) throw new TypeError('sequence must be an Array')
+  if (typeof distance !== 'number') throw new TypeError('distance must be a Number')
+
+  const head = sequence.slice(0, -distance)
+  const tail = sequence.slice(sequence.length - distance)
+
+  return tail.concat(head)
+}
+
+/**
+ * > In signal processing, cross-correlation is a measure of similarity of two
+ * > series as a function of the lag of one relative to the other.
+ * >
+ * > [...]
+ * >
+ * > In an autocorrelation, which is the cross-correlation of a signal with
+ * > itself, there will always be a peak at a lag of zero, and its size will be
+ * > the signal power.
+ *
+ * @param {number} sequenceA - The reference sequence.
+ * @param {number} sequenceB - The sequence to correlate.
+ * @returns {number[]} The correlation at each offset of `sequenceB`.
+ *
+ * @example <caption>Correlating a sequence with itself (autocorrelation)</caption>
+ *   cross_correlate(0b110010, 0b110010)
+ *   //=> [6, -2, -2, 2, -2, -2]
+ *
+ * @see https://en.wikipedia.org/wiki/Cross-correlation
+ */
+function cross_correlate(sequenceA, sequenceB) {
+  const length = Math.max(sequenceA.toString(2).length,
+                          sequenceB.toString(2).length)
+
+  // Map `sequenceB` into an `Array` so it can be rotated.
+  let bitsB = []
+  // @see http://stackoverflow.com/questions/14104208/convert-integer-to-binary-and-store-it-in-an-integer-array-of-specified-sizec#comment19514125_14104263
+  for (let i = 0; i < length; i++) bitsB[length - i] = (sequenceB >> i) & 0b1
+
+  let correlations = [dot_product(sequenceA, sequenceB, length)]
+  for (let offset = 1; offset < length; offset++) {
+    let rotated = rotate_sequence(bitsB, offset)
+
+    let sequence = parseInt(rotated.join(''), 2)
+    let correlation = dot_product(sequenceA, sequence, length)
+
+    correlations.push(correlation)
+  }
+
+  return correlations
+}
+export default cross_correlate
---
+++ test/utilities/cross-correlate.spec.js
@@ -0,0 +1,44 @@
+import 'babel-polyfill'
+import assert from 'assert'
+import described_function from '../../src/utilities/cross-correlate'
+import GoldCode from '../../src/gold-code'
+import MSequence from '../../src/m-sequence'
+
+describe('cross_correlate', () => {
+  it('autocorrelates', () => {
+    // @see http://pages.hmc.edu/harris/class/e11/lect7.pdf, slide 27
+    let correlation = described_function(0b110010, 0b110010)
+    let reference = [6, -2, -2, 2, -2, -2]
+
+    assert.deepEqual(correlation, reference)
+  })
+
+  it('autocorrelates an LFSR', () => {
+    // @see http://pages.hmc.edu/harris/class/e11/lect7.pdf, slide 28
+    let lfsr = new MSequence(5)
+
+    let reference = [lfsr.maximum_sequence_length]
+    for (let i = 1; i < lfsr.maximum_sequence_length; i++) reference.push(-1)
+
+    let correlation = described_function(lfsr.sequence, lfsr.sequence)
+
+    assert.deepEqual(correlation, reference)
+  })
+
+  it('cross-correlates Gold Codes', () => {
+    // @see http://pages.hmc.edu/harris/class/e11/lect7.pdf, slide 32
+    let gold_codeA = new GoldCode(5, [5, 2, 3, 4], [5, 3], 0b1)
+    let gold_codeB = new GoldCode(5, [5, 2, 3, 4], [5, 3], 0b10)
+
+    let reference = [-1, -1, -1, -9, -9, -9, -1, 7, 7, -9, -1, 7, 7, -1, -1, -1,
+                     7, 7, -9, -9, -1, 7, -1, -9, -1, 7, 7, 7, -1, -1, -1]
+
+    // @note The points on the graph are displayed in reverse order from the
+    //   list of Gold Code functions, so the sequences are passed in reverse
+    //   to compensate.
+    let correlation = described_function(gold_codeB.sequence,
+                                         gold_codeA.sequence)
+
+    assert.deepEqual(correlation, reference)
+  })
+})
