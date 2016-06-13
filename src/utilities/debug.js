4f7fe7d749e5eb0e5b582a063a0c6949f7bbf400
Jordan Cole <stilist@ratafia.info>
2016-06-12 18:58:50 -0700

    Add log_taps debug utility

--- src/utilities/debug.js
+++ src/utilities/debug.js
@@ -26,3 +26,58 @@ export function bits_to_string(number, length = bits_in_number) {
 
   return pad_bits(number.toString(2), length)
 }
+
+/**
+ * Replaces character `index` of `string` with `character`.
+ *
+ * @param {string} string - The string to splice.
+ * @param {number} index - The offset of the character to replace.
+ * @param {string} character - The replacement character.
+ * @returns {string} The spliced string.
+ *
+ * @access private
+ *
+ * @example
+ * splice_string('abcde', 1, '-')
+ * //=> 'a-cde'
+ */
+function splice_string(string, index, character = '') {
+  return string.substr(0, index) + character +
+    string.substr(index + character.length)
+}
+
+/**
+ * Logs an LFSR's state and marks which tap positions are used to determine
+ * the next state.
+ *
+ * @param {Array} taps - The tap positions to mark, ordered `m..1`.
+ * @param {number} current_state - The current state of the LFSR.
+ * @param {number} length - The number of bits to log.
+ * @returns {string} The string that was logged.
+ *
+ * @throws {TypeError}
+ *
+ * @example
+ * log_taps([5, 3, 2], 0b1, 5)
+ * //=> '00001\n• •• '
+ *
+ * @todo Handle `length` < the high bit of `current_state` or the largest
+ *   number in `taps`.
+ */
+export function log_taps(taps, current_state, length) {
+  if (!Array.isArray(taps)) throw new TypeError('array must be an Array')
+  if (!Number.isInteger(current_state)) throw new TypeError('current_state must be a Number')
+  if (!Number.isInteger(length)) throw new TypeError('length must be a Number')
+
+  length = Math.abs(length)
+
+  const indicators = taps.reduce((memo, tap_j) => splice_string(memo, length - tap_j, '•'),
+                                 ' '.repeat(length))
+
+  const output = `${bits_to_string(current_state, length)}
+${indicators}`
+
+  console.log(output)
+
+  return output
+}
--- test/utilities/debug.spec.js
+++ test/utilities/debug.spec.js
@@ -25,3 +25,32 @@ describe('bits_to_string', () => {
     assert.equal(result, '1111111111')
   })
 })
+
+describe('log_taps', () => {
+  it('throws an error if taps is not an Array', () => {
+    assert.throws(() => { described.log_taps(1) },
+                  TypeError)
+  })
+
+  it('throws an error if current_state is not a Number', () => {
+    assert.throws(() => { described.log_taps([], '1') },
+                  TypeError)
+  })
+
+  it('throws an error if length is not a Number', () => {
+    assert.throws(() => { described.log_taps([], 1, '1') },
+                  TypeError)
+  })
+
+  it('logs taps', () => {
+    let result = described.log_taps([5], 0b10, 5)
+
+    assert.equal(result, `00010\n•    `)
+  })
+
+  it('adjusts a negative length', () => {
+    let result = described.log_taps([5], 0b10, -5)
+
+    assert.equal(result, `00010\n•    `)
+  })
+})
